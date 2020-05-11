996f7f990b13dedf62f2d5766a4d9f2bb9902216
Simon Sundin <simonsundinsmail@gmail.com>
2020-05-11 16:22:02 +0200

    Laddat ner firebase

--- Piratspelet3.0/public/ModelsAndConfig/locationFunctions.js
+++ Piratspelet3.0/public/ModelsAndConfig/locationFunctions.js
@@ -1,22 +1,53 @@
-import React from "react";
 import firebaseConfig from "./mapsConfig";
-import firebase from "firebase";
+var firebase = require('firebase');
 
 
 
-  function setLocation(location){
-      var location = location;
+export class MapsModel{
+  constructor(props) {
+    super();
+    this.props = props;
+    this.subscribers = [];
+    this.myLocation;
+    this.user ="";
+    firebase.initializeApp(firebaseConfig);
+    this.db = firebase.database();
+  }
+
+  addObserver(callback) {
+    this.subscribers.push(callback);
+  }
+
+  removeObserver(callback) {
+    callback = this.subscribers.filter((o) => o !== callback);
+  }
+
+  notifyObservers(whatHappened) {
+    this.subscribers.forEach(function (callback) {
+      callback(whatHappened);
+    });
+  }
+
+  setLocation(location){
+      this.myLocation = location;
   }
 
   getLocation(){
-      location
+      return this.myLocation
   }
 
+  setUser(user){
+    this.user=user;
+  }
+
+  getUser(){
+    return this.user
+  }
 
   getOthersPlaylistsfromdatabase(limit = 5) {
     let locations = [];
     return this.db
-      .ref("piratspelet-6afa4/locations")
+      .ref("piratspelet/locations")
       .limitToLast(limit)
       .once("value")
       .then((snapshot) => {
@@ -37,7 +68,7 @@ import firebase from "firebase";
 
   //add a playlist to firebase
   addYourplaylistToDatabase(location, text, user, timestamp) {
-    this.db.ref("piratspelet-6afa4/locations" + user).set({
+    this.db.ref("piratspelet/location/'" + user).set({
       Location: location,
       Text: text,
       User: user,
@@ -48,25 +79,3 @@ import firebase from "firebase";
 
 }
 
-//A funktion for shuffleing an array (NOT USED)
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
-function uniq(a) {
-  var prims = { boolean: {}, number: {}, string: {} },
-    objs = [];
-
-  return a.filter(function (item) {
-    var type = typeof item;
-    if (type in prims)
-      return prims[type].hasOwnProperty(item)
-        ? false
-        : (prims[type][item] = true);
-    else return objs.indexOf(item) >= 0 ? false : objs.push(item);
-  });
-}
--- Piratspelet3.0/public/mapsscript.js
+++ Piratspelet3.0/public/mapsscript.js
@@ -1,8 +1,13 @@
+import MapsModel from "./ModelsAndConfig/locationFunctions.js";
+const locationfunctions = new MapsModel();
+
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('googlemap'), {
         center: { lat: 59.3498092, lng: 18.0684758 },
         zoom: 15,
-        mapTypeId: 'satellite',
+        styles:[{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#FFAD00"},{"saturation":50.2},{"lightness":-34.8},{"gamma":1}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"color":"#cbb42e"},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#FFC300"},{"saturation":54.2},{"lightness":-14.4},{"gamma":1}]},{"featureType":"road.highway","elementType":"all","stylers":[{"hue":"#FFAD00"},{"saturation":-19.8},{"lightness":-1.8},{"gamma":1}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"hue":"#FFAD00"},{"saturation":72.4},{"lightness":-32.6},{"gamma":1}]},{"featureType":"road.local","elementType":"all","stylers":[{"hue":"#FFAD00"},{"saturation":74.4},{"lightness":-18},{"gamma":1}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#00FFA6"},{"saturation":-63.2},{"lightness":38},{"gamma":1}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffe59c"}]}],
+
         disableDefaultUI: true //turns off default buttons
     });
     map.setTilt(45);
@@ -23,31 +28,29 @@ function initMap() {
   
   google.maps.event.addDomListener(currentlocation, 'click', function() {
     getLocation();
-    }); 
-  
-  google.maps.event.addDomListener(requestFullscreen,"click", function() {
-    fullscreenmap();
-    });
-    
+    });  
   
   }
+
   
   var elem = document.getElementById("myDiv")
   
-  function fullscreenmap(){
-    if (document.fullscreenEnabled) {
-      elem.requestFullscreen();
-      console.log("Du ska vara i fullscreen");
-      // supported
-      }
-    else{
-      alert("Sorry, browser does not support fullscreen!");
-      }
-  }
-  
+
   //kan användas för att ta sig ur en fullscreen
-  function closeFullscreen(){
-    document.exitFullscreen();
+
+
+  function postMyLocation(){
+    
+    if(navigator.geolocation){
+      let timestamp = new Date().toLocaleString();
+      let position = navigator.geolocation.getCurrentPosition(getPosition);
+      navigator.geolocation.getCurrentPosition(showPosition);
+      locationfunctions.setLocation(position);
+      locationfunctions.addYourplaylistToDatabase(position, "test", "test", timestamp);
+      
+   } else{
+      alert("Sorry, browser does not support geolocation!");
+   }
   }
   
   function favoriteLocation (id){
@@ -68,6 +71,11 @@ function initMap() {
       placeMarker(position , "Minas fovvoställe")
     }
   }
+  function getPosition(position){
+    position = { lat: position.coords.latitude, lng: position.coords.longitude };
+    return position
+  }
+
   function showPosition(position){
     position = { lat: position.coords.latitude, lng: position.coords.longitude };
     map.setCenter(position)
