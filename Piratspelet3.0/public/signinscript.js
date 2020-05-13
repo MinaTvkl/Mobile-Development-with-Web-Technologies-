807efb1b383677f27a70824b55e8765a009b7c44
Simon Sundin <simonsundinsmail@gmail.com>
2020-05-13 16:32:15 +0200

    Försök på form

--- Piratspelet3.0/public/signinscript.js
+++ Piratspelet3.0/public/signinscript.js
@@ -19,7 +19,7 @@ if (firebase.auth().currentUser === null){
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       
